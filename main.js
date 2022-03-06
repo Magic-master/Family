f6cb6ac01fe164ec4a519edfe6f8d49d7bb5f28f
Magic-master <85842164+Magic-master@users.noreply.github.com>
2022-03-06 13:03:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -17,4 +17,23 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3M5GUspeV/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('model Loaded!');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function goResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3) * 100 + "%";
+    }
+}
